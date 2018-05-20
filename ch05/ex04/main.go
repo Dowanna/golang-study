3e41e7c0b1f90b0ba53e05cbd20dff66fe5aa3f2
shunya matsubara <dowanna@shunyanoMacBook-Pro.local>
2018-05-21 07:59:54 +0900

    ex5.3

---
+++ ch05/ex04/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	. "github.com/golang-study/ch05/errorHandling"
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	CheckError(err)
+	for _, list := range Visit(nil, doc) {
+		fmt.Printf("%s\n", list)
+	}
+}
+
+var targetAttributes = map[string]string{
+	"a":      "href",
+	"script": "src",
+	"img":    "src",
+	"link":   "href",
+}
+
+func Visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+
+	if n.Type == html.ElementNode {
+		a := getAttribute(n, targetAttributes[n.Data])
+		if a != "" {
+			links = append(links, a)
+		}
+	}
+
+	links = Visit(links, n.FirstChild)
+	return Visit(links, n.NextSibling)
+}
+
+func getAttribute(n *html.Node, attr string) string {
+	for _, a := range n.Attr {
+		if a.Key == attr {
+			return a.Val
+		}
+	}
+	return ""
+}
